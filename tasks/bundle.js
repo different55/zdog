69fd09b4b78d2484bd2e3745ce7ae4b992a37df9
Different55 <burritosaur@protonmail.com>
2025-01-13 21:05:43 -0500

    Fix broken regex for bundling

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -4,9 +4,19 @@ const execSync = require('child_process').execSync;
 // get file paths from index.js
 const indexPath = 'js/index.js';
 let indexSrc = fs.readFileSync( `./${indexPath}`, 'utf8' );
-let cjsBlockRegex = /module\.exports = factory\([\w ,'.\-()/\n]+;/i;
+// Updated regex to match the actual format
+let cjsBlockRegex = /module\.exports = factory\(([\s\S]*?)\);/i;
 let cjsBlockMatch = indexSrc.match( cjsBlockRegex );
+
+if (!cjsBlockMatch) {
+  throw new Error('Could not find CommonJS module exports in index.js');
+}
+
 let paths = cjsBlockMatch[0].match( /require\('([.\-/\w]+)'\)/gi );
+
+if (!paths) {
+  throw new Error('Could not find any require statements in module exports');
+}
 
 paths = paths.map( function( path ) {
   return path.replace( "require('.", 'js' ).replace( "')", '.js' );
