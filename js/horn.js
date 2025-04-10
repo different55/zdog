3005e99401f2711da7854c026bc3cc94b0c43a9b
Different55 <burritosaur@protonmail.com>
2025-04-09 23:37:27 -0400

    Split Horn into Horn and Funnel.
    The Horn's eccentricity calculations *mostly* only make sense for
    circular endcaps, not spherical ones. I want to develop the Funnel
    further and also improve the Horn's surface approximation, so I've
    split them into two.
    
    The Funnel's surface now supports strokes, respects fill settings,
    and the Funnel itself now uses circular endcaps rather than
    spherical ones. I think the eccentricity calculations could still
    be improved but that's a lot of math for right now.
    
    The Horn's surface no longer connects the equators of each cap, as
    it's common for a larger cap to poke visibly through the surface.
    Eccentricity has been removed from its surface calculations, and the
    surface now fits to the tangets between the two spheres, with no
    visible dicontinuities.

--- js/horn.js
+++ js/horn.js
@@ -23,6 +23,8 @@ function noop() {}
 
 var HornGroup = Group.subclass({
   color: '#333',
+  fill: true,
+  stroke: true,
   updateSort: true,
 });
 
@@ -30,19 +32,15 @@ HornGroup.type = 'HornGroup';
 
 HornGroup.prototype.create = function() {
   Group.prototype.create.apply( this, arguments );
-  
+
   // vectors used for calculation
   this.renderApex = new Vector();
-  this.tangentFrontA = new Vector();
-  this.tangentFrontB = new Vector();
-  this.tangentRearA = new Vector();
-  this.tangentRearB = new Vector();
-  
+
   this.pathCommands = [
     new PathCommand( 'move', [ {} ] ),
     new PathCommand( 'line', [ {} ] ),
-	new PathCommand( 'line', [ {} ] ),
-	new PathCommand( 'line', [ {} ] ),
+    new PathCommand( 'line', [ {} ] ),
+    new PathCommand( 'line', [ {} ] ),
   ];
 };
 
@@ -57,84 +55,46 @@ HornGroup.prototype.renderHornSurface = function( ctx, renderer ) {
   }
   // render horn surface
   var elem = this.getRenderElement( ctx, renderer );
-  var frontBase = this.frontBase;
-  var frontDiameter = frontBase.stroke;
-  var rearBase = this.rearBase;
-  var rearDiameter = rearBase.stroke;
-  var scale = frontBase.renderNormal.magnitude();
-  var frontRadius = frontDiameter/2 * scale;
-  var rearRadius = rearDiameter/2 * scale;
-  
-  this.renderApex.set( rearBase.renderOrigin )
-    .subtract( frontBase.renderOrigin );
-  
-  // calculate tangents.
-  var scale = frontBase.renderNormal.magnitude();
+  var scale = this.addTo.renderNormal.magnitude();
+  var frontRadius = this.addTo.frontDiameter/2 * scale;
+  var rearRadius = this.addTo.rearDiameter/2 * scale;
+  this.renderApex.set( this.rearBase.renderOrigin )
+    .subtract( this.frontBase.renderOrigin );
+
   var apexDistance = this.renderApex.magnitude2d();
-  var normalDistance = frontBase.renderNormal.magnitude2d();
-  // eccentricity
-  var eccenAngle = Math.acos( normalDistance / scale );
-  var biggerRadius =  (frontRadius > rearRadius) ? frontRadius : rearRadius;
-  var eccenPercent;
-  if (frontRadius == 0 || rearRadius == 0) {
-	  eccenPercent = 1.0;
-  } else {
-	  eccenPercent = (Math.abs(frontRadius - rearRadius) / biggerRadius);
-  }
-  var eccen = Math.sin( eccenAngle ) * Math.sqrt(eccenPercent);
-  // does apex extend beyond eclipse of face
-  apexDistance = apexDistance + frontRadius/4 + rearRadius/4;
-  var isApexVisible = frontRadius * eccen < apexDistance &&
-                      rearRadius * eccen < apexDistance;
-  if ( !isApexVisible ) {
+  if ( apexDistance <= Math.abs( rearRadius - frontRadius ) ) {
     return;
   }
-  // update tangents
-  // TODO: try something more like horn_old.js updateSortValue()
-  var apexAngle = Math.atan2( frontBase.renderNormal.y, frontBase.renderNormal.x ) +
-      TAU/2;
-  var projectFrontLength = (apexDistance + frontRadius) / eccen;
-  var projectRearLength = (apexDistance + rearRadius) / eccen;
-  var projectFrontAngle = Math.acos( frontRadius / projectFrontLength );
-  var projectRearAngle = Math.acos( rearRadius / -projectRearLength );
-  // set tangent points
-  var tangentFrontA = this.tangentFrontA;
-  var tangentFrontB = this.tangentFrontB;
-  var tangentRearA = this.tangentRearA;
-  var tangentRearB = this.tangentRearB;
 
-  tangentFrontA.x = Math.cos( projectFrontAngle ) * frontRadius * eccen;
-  tangentFrontA.y = Math.sin( projectFrontAngle ) * frontRadius;
-  tangentRearA.x = Math.cos( projectRearAngle ) * rearRadius * eccen;
-  tangentRearA.y = Math.sin( projectRearAngle ) * rearRadius;
+  var angle = Math.atan2( this.renderApex.y, this.renderApex.x );
+  var angle2 = Math.acos((frontRadius - rearRadius) / apexDistance);
 
-  tangentFrontB.set( this.tangentFrontA );
-  tangentFrontB.y *= -1;
-  tangentRearB.set( this.tangentRearA );
-  tangentRearB.y *= -1;
+  var frontTangentA = this.pathCommands[0].renderPoints[0];
+  var rearTangentA = this.pathCommands[1].renderPoints[0];
+  var rearTangentB = this.pathCommands[2].renderPoints[0];
+  var frontTangentB = this.pathCommands[3].renderPoints[0];
 
-  tangentFrontA.rotateZ( apexAngle);
-  tangentFrontB.rotateZ( apexAngle);
-  tangentFrontA.add( frontBase.renderOrigin );
-  tangentFrontB.add( frontBase.renderOrigin );
-  tangentRearA.rotateZ( apexAngle + TAU/2);
-  tangentRearB.rotateZ( apexAngle + TAU/2);
-  tangentRearA.add( rearBase.renderOrigin );
-  tangentRearB.add( rearBase.renderOrigin );
-  
-  
-  // set path command render points
-  this.pathCommands[0].renderPoints[0].set( tangentFrontA );
-  this.pathCommands[1].renderPoints[0].set( tangentRearB );
-  this.pathCommands[2].renderPoints[0].set( tangentRearA );
-  this.pathCommands[3].renderPoints[0].set( tangentFrontB );
+  frontTangentA.x = Math.cos( angle + angle2 ) * frontRadius;
+  frontTangentA.y = Math.sin( angle + angle2 ) * frontRadius;
+  rearTangentA.x = Math.cos( angle + angle2 ) * rearRadius;
+  rearTangentA.y = Math.sin( angle + angle2 ) * rearRadius;
+
+  frontTangentB.x = Math.cos( angle - angle2 ) * frontRadius;
+  frontTangentB.y = Math.sin( angle - angle2 ) * frontRadius;
+  rearTangentB.x = Math.cos( angle - angle2 ) * rearRadius;
+  rearTangentB.y = Math.sin( angle - angle2 ) * rearRadius;
+
+  frontTangentA.add( this.frontBase.renderOrigin );
+  frontTangentB.add( this.frontBase.renderOrigin );
+  rearTangentA.add( this.rearBase.renderOrigin );
+  rearTangentB.add( this.rearBase.renderOrigin );
 
   if ( renderer.isCanvas ) {
     ctx.lineCap = 'butt'; // nice
   }
   renderer.renderPath( ctx, elem, this.pathCommands );
-  //renderer.stroke( ctx, elem, true, '#333', 0.1 ); // remove once testing is done.
-  renderer.fill( ctx, elem, true, this.color );
+  renderer.stroke( ctx, elem, this.stroke, this.color, Shape.prototype.getLineWidth.apply(this) );
+  renderer.fill( ctx, elem, this.fill, this.color );
   renderer.end( ctx, elem );
 
   if ( renderer.isCanvas ) {
@@ -151,6 +111,8 @@ HornGroup.prototype.getRenderElement = function( ctx, renderer ) {
   if ( !this.svgElement ) {
     // create svgElement
     this.svgElement = document.createElementNS( svgURI, 'path');
+    this.svgElement.setAttribute( 'stroke-linecap', 'round' );
+    this.svgElement.setAttribute( 'stroke-linejoin', 'round' );
   }
   return this.svgElement;
 };
@@ -189,6 +151,8 @@ Horn.prototype.create = function(/* options */) {
   this.group = new HornGroup({
     addTo: this,
     color: this.color,
+    fill: this.fill,
+    stroke: this.stroke,
     visible: this.visible,
   });
   var baseZ = this.length/2;
@@ -199,7 +163,7 @@ Horn.prototype.create = function(/* options */) {
     translate: { z: (baseZ - this.frontDiameter/2) },
     rotate: { y: TAU/2 },
     color: this.color,
-    stroke: this.frontDiameter,
+    stroke: this.frontDiameter + this.stroke,
     fill: this.fill,
     backface: this.frontFace || baseColor,
     visible: this.visible,
@@ -210,22 +174,22 @@ Horn.prototype.create = function(/* options */) {
     translate: { z: (-baseZ + this.rearDiameter/2) },
     rotate: { y: 0 },
     color: this.color,
-    stroke: this.rearDiameter,
+    stroke: this.rearDiameter + this.stroke,
     fill: this.fill,
     backface: baseColor,
     visible: this.visible,
   });
-  
+
 };
 
 Horn.prototype.updateFrontCapDiameter = function(size) {
-	this.frontBase.stroke = size;
+	this.frontBase.stroke = size + this.stroke;
 	var baseZ = this.length/2;
 	this.frontBase.translate.z = (baseZ - size/2);
 }
 
 Horn.prototype.updateRearCapDiameter = function(size) {
-	this.rearBase.stroke = size;
+	this.rearBase.stroke = size + this.stroke;
 	var baseZ = this.length/2;
 	this.rearBase.translate.z = (-baseZ + size/2);
 }
